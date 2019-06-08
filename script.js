9b2e0331b972c4badd0ff16a47bc189487472aa0
Aashish <aashish01official@gmail.com>
2019-06-08 16:23:56 +0530

    button features

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var buttons=document.getElementsByTagName("td");
-var color;
+var color; //to hold last color of button
 function changeColor(){
     color=this.style.backgroundColor;
     this.style.backgroundColor="rgb(252, 252, 196)";
@@ -11,3 +11,24 @@ for(var i=0; i<buttons.length;i++){
     buttons[i].addEventListener('mouseover',changeColor );
     buttons[i].addEventListener('mouseout',originalColor );
 }
+
+var str="";
+var numbers=document.getElementsByClassName("number");
+var operators=document.getElementsByClassName("operator");
+var clear=document.getElementById("AC");
+var negation=document.getElementById("+/-");
+var percent=document.getElementById("%");
+var equal=document.getElementById("equals");
+var decimal=document.getElementById(".");
+var output=document.getElementById("output");
+
+var print="";
+
+clear.addEventListener('click',function(){
+str="";
+print="";
+output.innerText=print;
+});
+
+
+
