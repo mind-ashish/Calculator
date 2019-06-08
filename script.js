ae831134564012af8817152389d07ff3328bb946
Aashish <aashish01official@gmail.com>
2019-06-08 15:58:53 +0530

    classes and id

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var buttons=document.getElementsByTagName("td");
 var color;
 function changeColor(){
     color=this.style.backgroundColor;
-    this.style.backgroundColor="rgb(248, 248, 206)";
+    this.style.backgroundColor="rgb(252, 252, 196)";
 }
 function originalColor(){
     this.style.backgroundColor=color;
